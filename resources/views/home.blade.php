a62d7ec4fcec2e47df1978ae614efc9fd6679541
muhamadalrifki26 <kikinion26@gmail.com>
2016-11-29 07:14:32 +0700

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+		<!-- BASICS -->
+        <meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Almuslim</title>
+        <meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="css/isotope.css" media="screen" />
+		<link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/bootstrap-theme.css">
+        <link rel="stylesheet" href="css/style.css">
+		<!-- skin -->
+		<link rel="stylesheet" href="skin/default.css">
+
+    </head>
+
+    <body>
+		<section id="header" class="appear"></section>
+		<div class="navbar navbar-fixed-top" role="navigation" data-0="line-height:100px; height:100px; background-color:rgba(0,0,0,0.3);" data-300="line-height:60px; height:60px; background-color:rgba(0,0,0,1);">
+			 <div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="fa fa-bars color-white"></span>
+					</button>
+					<h1><a class="navbar-brand" href="index.html" data-0="line-height:90px;" data-300="line-height:50px;">			ALMUSLIM
+					</a></h1>
+				</div>
+				<div class="navbar-collapse collapse">
+					<ul class="nav navbar-nav" data-0="margin-top:20px;" data-300="margin-top:5px;">
+						<li class="active"><a href="/">Home</a></li>
+
+					</ul>
+				</div><!--/.navbar-collapse -->
+			</div>
+		</div>
+
+		<section class="featured">
+			<div class="container">
+				<div class="row mar-bot40">
+					<div class="col-md-6 col-md-offset-3">
+
+						<div class="align-center">
+							<i class="fa fa-calendar fa-5x mar-bot20"></i>
+							<h2 class="slogan">Jadwal Sholat Hari Ini</h2>
+              <br>
+							<h3 style="color:#fff" id="lokasi">
+							</h3>
+              <p>
+                Jadwal sholat paling akurat sekomplek.</P>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- services -->
+		<section id="section-services" class="section pad-bot30 bg-white">
+		<div class="container">
+
+			<div class="row mar-bot40">
+				<div class="col-lg-4" >
+					<div class="align-center">
+						<i class="fa fa-clock-o fa-5x mar-bot20"></i>
+						<h4 class="text-bold">Subuh</h4>
+						<p id="waktusubuh"></p>
+					</div>
+				</div>
+
+				<div class="col-lg-4" >
+					<div class="align-center">
+						<i class="fa fa-clock-o fa-5x mar-bot20"></i>
+						<h4 class="text-bold">Dzuhur</h4>
+						<p id="waktudzuhur"></p>
+					</div>
+				</div>
+
+        <div class="col-lg-4" >
+					<div class="align-center">
+						<i class="fa fa-clock-o fa-5x mar-bot20"></i>
+						<h4 class="text-bold">Ashar</h4>
+						<p id="waktuashar"></p>
+					</div>
+				</div>
+
+        <div class="col-lg-6" >
+					<div class="align-center">
+						<i class="fa fa-clock-o fa-5x mar-bot20"></i>
+						<h4 class="text-bold">Maghrib</h4>
+						<p id="waktumaghrib"></p>
+					</div>
+				</div>
+
+				<div class="col-lg-6" >
+					<div class="align-center">
+						<i class="fa fa-clock-o fa-5x mar-bot20"></i>
+						<h4 class="text-bold">Isya</h4>
+						<p id="waktuisya"></p>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+		</section>
+		<!-- map -->
+
+	<section id="footer" class="section footer">
+		<div class="container">
+			<div class="row animated opacity mar-bot20" data-andown="fadeIn" data-animation="animation">
+				<div class="col-sm-12 align-center">
+                    <ul class="social-network social-circle">
+                        <li><a href="https://www.instagram.com/alrifki26/" class="icoInstagram" title="Rss"><i class="fa  fa-instagram"></i></a></li>
+                        <li><a href="https://www.facebook.com/muhamad.alrifki" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                    </ul>
+				</div>
+			</div>
+
+			<div class="row align-center copyright">
+					<div class="col-sm-12"><p>Copyright &copy; ALMUSLIM</p>
+                        <div class="credits">
+                            <!--
+                                All the links in the footer should remain intact.
+                                You can delete the links only if you purchased the pro version.
+                                Licensing information: https://bootstrapmade.com/license/
+                                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Amoeba
+                            -->
+                        </div>
+                    </div>
+			</div>
+		</div>
+
+	</section>
+	<a href="#header" class="scrollup"><i class="fa fa-chevron-up"></i></a>
+
+	<script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	<script src="https://maps.google.com/maps/api/js?sensor=true"></script>
+	<script src="js/jquery.isotope.min.js"></script>
+	<script src="js/jquery.nicescroll.min.js"></script>
+	<script src="js/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="js/skrollr.min.js"></script>
+	<script src="js/jquery.scrollTo-1.4.3.1-min.js"></script>
+	<script src="js/jquery.localscroll-1.2.7-min.js"></script>
+	<script src="js/stellar.js"></script>
+	<script src="js/jquery.appear.js"></script>
+    <script src="js/main.js"></script>
+    <script src="contactform/contactform.js"></script>
+
+    <script type="text/javascript">
+    jQuery(function($) {
+ $.getJSON('http://muslimsalat.com/daily.json?key={{env('MUSLIM_SALAT_KEY')}}&jsoncallback=?', function (times)
+ {
+   $("#lokasi").html(times.state + "," + times.country);
+   $("#waktusubuh").html(times.items[0].fajr);
+   $("#waktudzuhur").html(times.items[0].dhuhr);
+   $("#waktuashar").html(times.items[0].asr);
+   $("#waktumaghrib").html(times.items[0].maghrib);
+   $("#waktuisya").html(times.items[0].isha);
+ });
+  });
+    </script>
+
+	</body>
+</html>
